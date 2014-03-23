d0a2beea832db9932c513bdebedb98d09518b162
dian.wen <2010wendian@gmail.com>
2014-03-23 17:52:49 -0400

    deleted unused files

--- OLD/master.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "SuperHTML.php";
-$s = new SuperHTML("Basic Super Page");
-$s->buildTop();
-$s->buildBody();
-$s->addText($page_content);
-$s->buildBottom();
-print $s->getPage();
-?>
